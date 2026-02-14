51a5ca2a2dff36e6723c4e2bea870ed3a26c59b9
AIGI7 <kopylov.artm@gmail.com>
2026-02-14 14:01:52 +0300

    Add movement and Action handling

--- src/UniversityTasks.java
+++ src/UniversityTasks.java
@@ -11,7 +11,7 @@ public class UniversityTasks {
 // === ЭТАЖ 0: "Обрыв" (5x6) ===
             {
                     {'#', '#', '#', '#', '_', '_'},
-                    {'#', 'P', '_', '_', '_', '_'},
+                    {'#', '_', '_', '_', '_', '_'},
                     {'#', '_', '_', 'U', '_', '_'},
                     {'#', '_', '_', '_', '_', '_'},
                     {'#', '_', '_', '_', '_', '_'}
@@ -36,33 +36,42 @@ public class UniversityTasks {
 
         System.out.print("w/s/a/d ");
         while (true) {
+            printHud();
+            printFloor();
+
             String input = scanner.nextLine().toLowerCase();
             if (input.isEmpty()) continue;
+            char cmd = input.charAt(0);
+            if (cmd == 'e') {
+                handleAction(scanner);
+            }
+            else{
+                handleMove(cmd);
+            }
 
-            char dir = input.charAt(0);
-            printFloor(tower[floor]);
-            handleMove(dir);
         }
     }
 
     public static void handleMove(char dir){
 
-        if (playerY < 0 || playerY >= tower[floor].length || playerX < 0 || playerX >= tower[floor][playerY].length || floor < 0 || floor >= tower.length){
+        if (playerY < 0 || playerY >= tower[floor].length ||
+            playerX < 0 || playerX >= tower[floor][playerY].length ||
+            floor < 0 || floor >= tower.length){
             System.out.println("за пределы");
             return;
         }
 
         char playerPosition = 'P';
-       int nextStepSign = tower[floor][playerY][playerX];
+        int nextStepSign = tower[floor][playerY][playerX];
         char oldChar = 'P';
         char newChar = '_';
-        System.out.println("x: " + playerX + " y: " + playerY);
         switch (dir){
             case 'w':
                 System.out.println('w');
-                if (tower[floor][playerY - 1][playerX] != '#') {
+                if (tower[floor][playerY - 1][playerX] == '_') {
                     playerY -= 1;
                     positionCheck(tower);
+                    torch--;
                 }
                 break;
             case 'a':
@@ -87,12 +96,50 @@ public class UniversityTasks {
                 }
                 break;
         }
+    }
 
+    public static void handleAction(Scanner scanner) {
 
+        System.out.println("в какую сторону использовать? ");
+
+        String input = scanner.nextLine().toLowerCase();
+        if (input.isEmpty()) return;
+        char dir = input.charAt(0);
+        int demoX = playerX;
+        int demoY = playerY;
+        switch (dir){
+            case 'w': demoY--; break;
+            case 's': demoY++; break;
+            case 'a': demoX--; break;
+            case 'd': demoX++; break;
+        }
+        if (demoY < 0 || demoY > tower[floor].length ||
+            demoX < 0 || demoX > tower[floor][demoY].length){
+
+            System.out.print("за границей");
+        }
+        char tile = tower[floor][demoY][demoX];
+        if (tile == 'U'){
+            floor++;
+            playerX = demoX;
+            playerY = demoY;
+            System.out.println("вы поднялись на этаж " + floor);
+        }
+        else if (tile == 'D'){
+            floor--;
+            playerX = demoX;
+            playerY = demoY;
+            System.out.println("вы спустились на этаж " + floor);
+        }
+        else{
+            System.out.println("у нас нет лестницы");
+        }
 
     }
+
     public static void positionCheck(char[][][] tower){
         char nextStepSign = tower[floor][playerY][playerX];
+
         if (nextStepSign == '#') {
             System.out.println("стена");
         }
@@ -101,10 +148,22 @@ public class UniversityTasks {
         }
     }
 
-    public static void printFloor(char[][] floor){
-        for (int i = 0; i < floor.length; i++) {
-            for (int j = 0; j < floor[i].length; j++) {
-                System.out.print(floor[i][j] + " ");
+    public static void printHud(){
+        System.out.println("этаж " + floor);
+        System.out.println("x: " + playerX + " y: " + playerY);
+        System.out.println("факел " + torch);
+    }
+
+    public static void printFloor(){
+        char[][] currentFloor = tower[floor];
+        for (int i = 0; i < currentFloor.length; i++) {
+            for (int j = 0; j < currentFloor[i].length; j++) {
+                if (i == playerY && j == playerX) {
+                    System.out.print("P ");
+                }
+                else {
+                    System.out.print(currentFloor[i][j] + " ");
+                }
             }
             System.out.println();
         }
